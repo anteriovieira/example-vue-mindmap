9abe58d88f1771e46b17a5edc4108a62bfa34e33
Antério Vieira <anteriovieira@gmail.com>
2018-01-12 09:06:06 -0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		<mind-map
+			:nodes="nodes"
+			:connections="connections"
+			:editable="true"
+		/>
+	</div>
+</template>
+
+<script>
+	import map from './map'
+
+	console.log(map)
+
+	export default {
+    name: 'app',
+		data() {
+			return {...map}
+		}
+  }
+</script>
